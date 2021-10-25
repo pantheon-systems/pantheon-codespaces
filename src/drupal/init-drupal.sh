27772c256015ffb61e8a204201230e70a4ee0e7e
Tyler Fahey <twfahey1@gmail.com>
2021-10-25 20:20:00 +0000

    Add d8 framework

--- src/drupal/init-drupal.sh
+++ src/drupal/init-drupal.sh
@@ -1,2 +1,2 @@
 # Copy over local settings to Drupal codebase.
-cp /workspaces/$(echo $RepositoryName)/.devcontainer/src/drupal/settings.local.php /workspaces/$(echo $RepositoryName)/web/sites/default/settings.local.php
+cp /workspaces/$(echo $RepositoryName)/.devcontainer/src/drupal/settings.local.php /workspaces/$(echo $RepositoryName)/sites/default/settings.local.php
