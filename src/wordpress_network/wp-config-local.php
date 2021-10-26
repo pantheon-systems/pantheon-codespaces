8beb5faf45606c3eeb3f03ae5d0958d019b3d073
jspellman814 <john.spellman@pantheon.io>
2021-10-26 15:57:33 -0600

    Use CODESPACE_URL variable in wp-config

--- src/wordpress_network/wp-config-local.php
+++ src/wordpress_network/wp-config-local.php
@@ -25,7 +25,7 @@ define('DB_COLLATE', '');
 
 define( 'MULTISITE', true );
 define( 'SUBDOMAIN_INSTALL', false );
-define( 'DOMAIN_CURRENT_SITE', 'https://' . $CODESPACE_NAME . '-8080.githubpreview.dev' );
+define( 'DOMAIN_CURRENT_SITE', $CODESPACE_URL );
 define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );
